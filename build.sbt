45bb8ab7c88b62180b2a3fafa13b7ad16e07c3dd
Thomas Feng <tfeng@linkedin.com>
2014-08-09 01:11:59 -0700

    Bump version after release.

--- build.sbt
+++ build.sbt
@@ -5,26 +5,3 @@ Settings.common ++ Settings.disablePublishing
 lazy val parent = project in file(".") aggregate(dust)
 
 lazy val dust = project in file("plugins/dust") enablePlugins(SbtWeb)
-
-pomExtra :=
-  <developers>
-    <developer>
-      <email>tfeng@berkeley.edu</email>
-      <name>Thomas Feng</name>
-      <url>https://github.com/tfeng</url>
-      <id>tfeng</id>
-    </developer>
-  </developers>
-  <url>https://github.com/tfeng/sbt-plugins</url>
-  <scm>
-    <url>https://github.com/tfeng/sbt-plugins</url>
-    <connection>scm:git:https://github.com/tfeng/sbt-plugins.git</connection>
-    <developerConnection>scm:git:git@github.com:tfeng/sbt-plugins.git</developerConnection>
-  </scm>
-  <licenses>
-    <license>
-      <name>Apache 2</name>
-      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-      <distribution>repo</distribution>
-    </license>
-  </licenses>
--- project/Settings.scala
+++ project/Settings.scala
@@ -6,7 +6,29 @@ import Keys._
 object Settings {
   val common: Seq[Setting[_]] = Seq(
     organization := "me.tfeng.sbt-plugins",
-    version := "0.1.1-SNAPSHOT"
+    version := "0.1.2-SNAPSHOT",
+    pomExtra :=
+      <developers>
+        <developer>
+          <email>tfeng@berkeley.edu</email>
+          <name>Thomas Feng</name>
+          <url>https://github.com/tfeng</url>
+          <id>tfeng</id>
+        </developer>
+      </developers>
+      <url>https://github.com/tfeng/sbt-plugins</url>
+      <scm>
+        <url>https://github.com/tfeng/sbt-plugins</url>
+        <connection>scm:git:https://github.com/tfeng/sbt-plugins.git</connection>
+        <developerConnection>scm:git:git@github.com:tfeng/sbt-plugins.git</developerConnection>
+      </scm>
+      <licenses>
+        <license>
+          <name>Apache 2</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
   )
 
   val disablePublishing: Seq[Setting[_]] = Seq(
