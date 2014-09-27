e91cea61f7bcab57490c3b4b97a07bb4fdb369f8
Thomas Feng <tfeng@linkedin.com>
2014-09-27 16:54:10 -0700

    Release 0.2.2.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,11 +2,11 @@ package me.tfeng.sbt.plugins
 
 object Versions {
 
-  val project = "0.2.2-SNAPSHOT"
+  val project = "0.2.2"
 
   val avro = "1.7.7"
 
   val dustjs = "2.4.0-1"
 
-  val webjarsLocator = "0.18"
+  val webjarsLocator = "0.19"
 }
