0906ddc6f25e74182dfa11b1d38f693de94e098a
Thomas Feng <tfeng@linkedin.com>
2015-05-25 21:32:44 -0700

    Upgrade external dependencies.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@ package me.tfeng.sbt.plugins
 
 object Versions {
 
-  val project = "0.3.8"
+  val project = "0.4.0-SNAPSHOT"
 
   val avro = "1.7.7"
 
-  val dustjs = "2.4.0-1"
+  val dustjs = "2.6.1"
 
   val guava = "18.0"
 
-  val webjarsLocator = "0.21"
+  val webjarsLocator = "0.23"
 }
