de94ff2dceae31bf82ab0d9d30aa8bb3a2f8c190
Thomas Feng <tfeng@linkedin.com>
2016-02-21 18:48:57 -0800

    Upgrade dependencies.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,15 +2,15 @@ package me.tfeng.sbt.plugins
 
 object Versions {
 
-  val project = "0.6.0"
+  val project = "0.7.0-SNAPSHOT"
 
-  val avro = "1.7.7"
+  val avro = "1.8.0"
 
-  val dustjs = "2.6.1"
+  val dustjs = "2.6.1-1"
 
   val guava = "19.0"
 
   val scala = "2.10.5"
 
-  val webjarsLocator = "0.28"
+  val webjarsLocator = "0.30"
 }
